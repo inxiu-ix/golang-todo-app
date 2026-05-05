2202db6a193b0ac27247093baac216ca51ca7499
Alexander Mikheev <a.inxiu.m@gmail.com>
2026-05-05 19:39:12 +0300

    fixes

--- internal/features/web/transport/http/get_main_page.go
+++ internal/features/web/transport/http/get_main_page.go
@@ -4,9 +4,30 @@ import (
 	"net/http"
 	"os"
 	"path"
+
+	core_logger "github.com/inxiu-ix/golang-todo-app/internal/core/logger"
+	core_http_response "github.com/inxiu-ix/golang-todo-app/internal/core/transport/http/response"
 )
 
 func (h *WebHTTPHandler) GetMainPage(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	log := core_logger.FromContext(ctx)
+	responseHandler := core_http_response.NewHTTPResponseHandler(log, w)
+
 	filePath := path.Join(os.Getenv("PROJECT_ROOT"), "public/index.html")
-	http.ServeFile(w, r, filePath)
+
+	f, err := os.Open(filePath)
+	if err != nil {
+		responseHandler.ErrorResponse(err, "failed to open main page")
+		return
+	}
+	defer f.Close()
+
+	fi, err := f.Stat()
+	if err != nil {
+		responseHandler.ErrorResponse(err, "failed to stat main page")
+		return
+	}
+
+	http.ServeContent(w, r, "index.html", fi.ModTime(), f)
 }
